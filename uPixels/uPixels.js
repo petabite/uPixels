8772554968f5c96dac0ec33d6da2b410118810e3
petabite <jmslca123@gmail.com>
2019-05-19 17:15:33 -0700

    implement animations + custom tabs

--- uPixels/uPixels.js
+++ uPixels/uPixels.js
@@ -1,3 +1,4 @@
+var brightnessSlider, startingPositionSlider, segmentLengthSlider
 $(document).ready(function(){
   $("#colorpicker").spectrum({
     color: "rgb(0, 255, 155)",
@@ -8,13 +9,25 @@ $(document).ready(function(){
       console.log($(this).spectrum('get').toRgb());
     }
   });
+  $("#second-colorpicker").spectrum({
+    color: "rgb(0, 171, 255)",
+    preferredFormat: 'rgb',
+    showButtons: false,
+    showInput: true,
+    containerClassName: 'second-colorpicker',
+    change: function(color) {
+      console.log($(this).spectrum('get').toRgb());
+    }
+  });
+
   M.AutoInit();
-  var slider = document.getElementById('brightness-slider');
-  noUiSlider.create(slider, {
+  $('.tabs').tabs({'swipeable':true});
+
+  brightnessSlider = document.getElementById('brightness-slider');
+  noUiSlider.create(brightnessSlider, {
    start: 50,
-   connect: true,
    step: 1,
-   orientation: 'horizontal', // 'horizontal' or 'vertical'
+   behavior: 'drag-tap',
    range: {
      'min': 0,
      'max': 100
@@ -23,4 +36,40 @@ $(document).ready(function(){
      decimals: 0
    })
   });
+
+  brightnessSlider.noUiSlider.on('update', function (brightness) {
+    $('#brightness-label').text(brightness);
+  })
+
+  startingPositionSlider = document.getElementById('starting-position-slider');
+  noUiSlider.create(startingPositionSlider, {
+   start: 1,
+   step: 1,
+   behavior: 'drag-tap',
+   range: {
+     'min': [0],
+     'max': [300]
+   },
+   format: wNumb({
+     decimals: 0
+   })
+  });
+
+  startingPositionSlider.noUiSlider.on('update', function (position) {
+    $('#position-label').text(position);
+  })
+
+  // segmentLengthSlider = document.getElementById('segment-length-slider');
+  // noUiSlider.create(segmentLengthSlider, {
+  //  start: 50,
+  //  step: 1,
+  //  behavior: 'drag-tap',
+  //  range: {
+  //    'min': [1],
+  //    'max': [300]
+  //  },
+  //  format: wNumb({
+  //    decimals: 0
+  //  })
+  // });
 });
